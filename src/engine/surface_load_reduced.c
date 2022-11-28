78164cca5f00270eb3090922c7a6f15c524ff08d
Wargh <nafoizret@web.de>
2022-11-28 07:58:57 +0100

    Create surface_load_reduced.c

---
+++ src/engine/surface_load_reduced.c
@@ -0,0 +1,454 @@
+#include <PR/ultratypes.h>
+
+#include "sm64.h"
+#include "game/ingame_menu.h"
+#include "graph_node.h"
+#include "behavior_script.h"
+#include "behavior_data.h"
+#include "game/memory.h"
+#include "game/object_helpers.h"
+#include "game/macro_special_objects.h"
+#include "surface_collision.h"
+#include "game/mario.h"
+#include "game/object_list_processor.h"
+#include "surface_load.h"
+
+/**
+ * Iterates through the entire partition, clearing the surfaces.
+ */
+static void clear_spatial_partition(SpatialPartitionCell *cells) {
+    register s32 i = NUM_CELLS * NUM_CELLS;
+
+    while (i--) {
+        (*cells)[SPATIAL_PARTITION_FLOORS].next = NULL;
+        (*cells)[SPATIAL_PARTITION_CEILS].next = NULL;
+        (*cells)[SPATIAL_PARTITION_WALLS].next = NULL;
+
+        cells++;
+    }
+}
+
+/**
+ * Clears the static (level) surface partitions for new use.
+ */
+void clear_static_surfaces(void) {
+    clear_spatial_partition(&gStaticSurfacePartition[0][0]);
+}
+
+/**
+ * Allocate the part of the surface node pool to contain a surface node.
+ */
+#include <stdlib.h>
+static struct SurfaceNode *alloc_surface_node(void) {
+    struct SurfaceNode *node = malloc(sizeof(struct SurfaceNode));
+
+    node->next = NULL;
+    
+    return node;
+}
+
+struct Surface *alloc_surface(void) {
+    struct Surface *surface = malloc(sizeof(struct Surface));
+
+    surface->type = 0;
+    surface->force = 0;
+    surface->flags = 0;
+    surface->room = 0;
+    surface->object = NULL;
+
+    return surface;
+}
+
+/**
+ * Add a surface to the correct cell list of surfaces.
+ * @param dynamic Determines whether the surface is static or dynamic
+ * @param cellX The X position of the cell in which the surface resides
+ * @param cellZ The Z position of the cell in which the surface resides
+ * @param surface The surface to add
+ */
+static void add_surface_to_cell(s16 dynamic, s16 cellX, s16 cellZ, struct Surface *surface) {
+    struct SurfaceNode *newNode = alloc_surface_node();
+    struct SurfaceNode *list;
+    s16 surfacePriority;
+    s16 priority;
+    s16 sortDir;
+    s16 listIndex;
+
+    if (surface->normal.y > 0.01) {
+        listIndex = SPATIAL_PARTITION_FLOORS;
+        sortDir = 1; // highest to lowest, then insertion order
+    } else if (surface->normal.y < -0.01) {
+        listIndex = SPATIAL_PARTITION_CEILS;
+        sortDir = -1; // lowest to highest, then insertion order
+    } else {
+        listIndex = SPATIAL_PARTITION_WALLS;
+        sortDir = 0; // insertion order
+
+        if (surface->normal.x < -0.707 || surface->normal.x > 0.707) {
+            surface->flags |= SURFACE_FLAG_X_PROJECTION;
+        }
+    }
+
+    //! (Surface Cucking) Surfaces are sorted by the height of their first
+    //  vertex. Since vertices aren't ordered by height, this causes many
+    //  lower triangles to be sorted higher. This worsens surface cucking since
+    //  many functions only use the first triangle in surface order that fits,
+    //  missing higher surfaces.
+    //  upperY would be a better sort method.
+    surfacePriority = surface->vertex1[1] * sortDir;
+
+    newNode->surface = surface;
+
+    if (dynamic) {
+        list = &gDynamicSurfacePartition[cellZ][cellX][listIndex];
+    } else {
+        list = &gStaticSurfacePartition[cellZ][cellX][listIndex];
+    }
+
+    // Loop until we find the appropriate place for the surface in the list.
+    while (list->next != NULL) {
+        priority = list->next->surface->vertex1[1] * sortDir;
+
+        if (surfacePriority > priority) {
+            break;
+        }
+
+        list = list->next;
+    }
+
+    newNode->next = list->next;
+    list->next = newNode;
+}
+
+/**
+ * Returns the lowest of three values.
+ */
+static s16 min_3(s16 a0, s16 a1, s16 a2) {
+    if (a1 < a0) {
+        a0 = a1;
+    }
+
+    if (a2 < a0) {
+        a0 = a2;
+    }
+
+    return a0;
+}
+
+/**
+ * Returns the highest of three values.
+ */
+static s16 max_3(s16 a0, s16 a1, s16 a2) {
+    if (a1 > a0) {
+        a0 = a1;
+    }
+
+    if (a2 > a0) {
+        a0 = a2;
+    }
+
+    return a0;
+}
+
+/**
+ * Every level is split into 16 * 16 cells of surfaces (to limit computing
+ * time). This function determines the lower cell for a given x/z position.
+ * @param coord The coordinate to test
+ */
+static s16 lower_cell_index(s16 coord) {
+    s16 index;
+
+    // Move from range [-0x2000, 0x2000) to [0, 0x4000)
+    coord += LEVEL_BOUNDARY_MAX;
+    if (coord < 0) {
+        coord = 0;
+    }
+
+    // [0, 16)
+    index = coord / CELL_SIZE;
+
+    // Include extra cell if close to boundary
+    //! Some wall checks are larger than the buffer, meaning wall checks can
+    //  miss walls that are near a cell border.
+    if (coord % CELL_SIZE < 50) {
+        index--;
+    }
+
+    if (index < 0) {
+        index = 0;
+    }
+
+    // Potentially > 15, but since the upper index is <= 15, not exploitable
+    return index;
+}
+
+/**
+ * Every level is split into 16 * 16 cells of surfaces (to limit computing
+ * time). This function determines the upper cell for a given x/z position.
+ * @param coord The coordinate to test
+ */
+static s16 upper_cell_index(s16 coord) {
+    s16 index;
+
+    // Move from range [-0x2000, 0x2000) to [0, 0x4000)
+    coord += LEVEL_BOUNDARY_MAX;
+    if (coord < 0) {
+        coord = 0;
+    }
+
+    // [0, 16)
+    index = coord / CELL_SIZE;
+
+    // Include extra cell if close to boundary
+    //! Some wall checks are larger than the buffer, meaning wall checks can
+    //  miss walls that are near a cell border.
+    if (coord % CELL_SIZE > CELL_SIZE - 50) {
+        index++;
+    }
+
+    if (index > NUM_CELLS_INDEX) {
+        index = NUM_CELLS_INDEX;
+    }
+
+    // Potentially < 0, but since lower index is >= 0, not exploitable
+    return index;
+}
+
+/**
+ * Every level is split into 16x16 cells, this takes a surface, finds
+ * the appropriate cells (with a buffer), and adds the surface to those
+ * cells.
+ * @param surface The surface to check
+ * @param dynamic Boolean determining whether the surface is static or dynamic
+ */
+void add_surface(struct Surface *surface, s32 dynamic) {
+    // minY/maxY maybe? s32 instead of s16, though.
+    UNUSED s32 unused1, unused2;
+    s16 minX, minZ, maxX, maxZ;
+
+    s16 minCellX, minCellZ, maxCellX, maxCellZ;
+
+    s16 cellZ, cellX;
+    // cellY maybe? s32 instead of s16, though.
+    UNUSED s32 unused3 = 0;
+
+    minX = min_3(surface->vertex1[0], surface->vertex2[0], surface->vertex3[0]);
+    minZ = min_3(surface->vertex1[2], surface->vertex2[2], surface->vertex3[2]);
+    maxX = max_3(surface->vertex1[0], surface->vertex2[0], surface->vertex3[0]);
+    maxZ = max_3(surface->vertex1[2], surface->vertex2[2], surface->vertex3[2]);
+
+    minCellX = lower_cell_index(minX);
+    maxCellX = upper_cell_index(maxX);
+    minCellZ = lower_cell_index(minZ);
+    maxCellZ = upper_cell_index(maxZ);
+
+    for (cellZ = minCellZ; cellZ <= maxCellZ; cellZ++) {
+        for (cellX = minCellX; cellX <= maxCellX; cellX++) {
+            add_surface_to_cell(dynamic, cellX, cellZ, surface);
+        }
+    }
+}
+
+/**
+ * Initializes a Surface struct using the given vertex data
+ * @param vertexData The raw data containing vertex positions
+ * @param vertexIndices Helper which tells positions in vertexData to start reading vertices
+ */
+static struct Surface *read_surface_data(s16 *vertexData, s16 **vertexIndices) {
+    struct Surface *surface;
+    register s32 x1, y1, z1;
+    register s32 x2, y2, z2;
+    register s32 x3, y3, z3;
+    s32 maxY, minY;
+    f32 nx, ny, nz;
+    f32 mag;
+    s16 offset1, offset2, offset3;
+
+    offset1 = 3 * (*vertexIndices)[0];
+    offset2 = 3 * (*vertexIndices)[1];
+    offset3 = 3 * (*vertexIndices)[2];
+
+    x1 = *(vertexData + offset1 + 0);
+    y1 = *(vertexData + offset1 + 1);
+    z1 = *(vertexData + offset1 + 2);
+
+    x2 = *(vertexData + offset2 + 0);
+    y2 = *(vertexData + offset2 + 1);
+    z2 = *(vertexData + offset2 + 2);
+
+    x3 = *(vertexData + offset3 + 0);
+    y3 = *(vertexData + offset3 + 1);
+    z3 = *(vertexData + offset3 + 2);
+
+    // (v2 - v1) x (v3 - v2)
+    nx = (y2 - y1) * (z3 - z2) - (z2 - z1) * (y3 - y2);
+    ny = (z2 - z1) * (x3 - x2) - (x2 - x1) * (z3 - z2);
+    nz = (x2 - x1) * (y3 - y2) - (y2 - y1) * (x3 - x2);
+    mag = sqrtf(nx * nx + ny * ny + nz * nz);
+
+    // Could have used min_3 and max_3 for this...
+    minY = y1;
+    if (y2 < minY) {
+        minY = y2;
+    }
+    if (y3 < minY) {
+        minY = y3;
+    }
+
+    maxY = y1;
+    if (y2 > maxY) {
+        maxY = y2;
+    }
+    if (y3 > maxY) {
+        maxY = y3;
+    }
+
+    // Checking to make sure no DIV/0
+    if (mag < 0.0001) {
+        return NULL;
+    }
+    mag = (f32)(1.0 / mag);
+    nx *= mag;
+    ny *= mag;
+    nz *= mag;
+
+    surface = alloc_surface();
+
+    surface->vertex1[0] = x1;
+    surface->vertex2[0] = x2;
+    surface->vertex3[0] = x3;
+
+    surface->vertex1[1] = y1;
+    surface->vertex2[1] = y2;
+    surface->vertex3[1] = y3;
+
+    surface->vertex1[2] = z1;
+    surface->vertex2[2] = z2;
+    surface->vertex3[2] = z3;
+
+    surface->normal.x = nx;
+    surface->normal.y = ny;
+    surface->normal.z = nz;
+
+    surface->originOffset = -(nx * x1 + ny * y1 + nz * z1);
+
+    surface->lowerY = minY - 5;
+    surface->upperY = maxY + 5;
+
+    return surface;
+}
+
+/**
+ * Returns whether a surface has exertion/moves Mario
+ * based on the surface type.
+ */
+static s32 surface_has_force(s16 surfaceType) {
+    s32 hasForce = FALSE;
+
+    switch (surfaceType) {
+        case SURFACE_0004: // Unused
+        case SURFACE_FLOWING_WATER:
+        case SURFACE_DEEP_MOVING_QUICKSAND:
+        case SURFACE_SHALLOW_MOVING_QUICKSAND:
+        case SURFACE_MOVING_QUICKSAND:
+        case SURFACE_HORIZONTAL_WIND:
+        case SURFACE_INSTANT_MOVING_QUICKSAND:
+            hasForce = TRUE;
+            break;
+
+        default:
+            break;
+    }
+    return hasForce;
+}
+
+/**
+ * Returns whether a surface should have the
+ * SURFACE_FLAG_NO_CAM_COLLISION flag.
+ */
+static s32 surf_has_no_cam_collision(s16 surfaceType) {
+    s32 flags = 0;
+
+    switch (surfaceType) {
+        case SURFACE_NO_CAM_COLLISION:
+        case SURFACE_NO_CAM_COLLISION_77: // Unused
+        case SURFACE_NO_CAM_COL_VERY_SLIPPERY:
+        case SURFACE_SWITCH:
+            flags = SURFACE_FLAG_NO_CAM_COLLISION;
+            break;
+
+        default:
+            break;
+    }
+
+    return flags;
+}
+
+/**
+ * Load in the surfaces for a given surface type. This includes setting the flags,
+ * exertion, and room.
+ */
+static void load_static_surfaces(s16 **data, s16 *vertexData, s16 surfaceType, s8 **surfaceRooms) {
+    s32 i;
+    s32 numSurfaces;
+    struct Surface *surface;
+    s8 room = 0;
+    s16 hasForce = surface_has_force(surfaceType);
+    s16 flags = surf_has_no_cam_collision(surfaceType);
+
+    numSurfaces = *(*data);
+    (*data)++;
+
+    for (i = 0; i < numSurfaces; i++) {
+        if (*surfaceRooms != NULL) {
+            room = *(*surfaceRooms);
+            (*surfaceRooms)++;
+        }
+
+        surface = read_surface_data(vertexData, data);
+        if (surface != NULL) {
+            surface->room = room;
+            surface->type = surfaceType;
+            surface->flags = (s8) flags;
+
+            if (hasForce) {
+                surface->force = *(*data + 3);
+            } else {
+                surface->force = 0;
+            }
+
+            add_surface(surface, FALSE);
+        }
+
+        *data += 3;
+        if (hasForce) {
+            (*data)++;
+        }
+    }
+}
+
+/**
+ * Read the data for vertices for reference by triangles.
+ */
+static s16 *read_vertex_data(s16 **data) {
+    s32 numVertices;
+    UNUSED u8 filler[16];
+    s16 *vertexData;
+
+    numVertices = *(*data);
+    (*data)++;
+
+    vertexData = *data;
+    *data += 3 * numVertices;
+
+    return vertexData;
+}
+
+/**
+ * If not in time stop, clear the surface partitions.
+ */
+void clear_dynamic_surfaces(void) {
+    if (!(gTimeStopState & TIME_STOP_ACTIVE)) {
+        clear_spatial_partition(&gDynamicSurfacePartition[0][0]);
+    }
+}
