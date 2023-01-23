c5377681e11986e5e96431be182d5b1d15b7573d
chaosBrick <1663221+chaosBrick@users.noreply.github.com>
2023-01-23 01:09:02 +0100

    Fix typo

--- bruteforce/framework/misc_util.h
+++ bruteforce/framework/misc_util.h
@@ -19,7 +19,7 @@ void init_dynamic_surfaces(Triangles tris);
 struct Surface *gen_surface(s16 x1, s16 y1, s16 z1, s16 x2, s16 y2, s16 z2, s16 x3, s16 y3, s16 z3, s16 surf_type);
 void adjust_analog_stick(struct Controller *controller);
 
-void intiCamera();
+void initCamera();
 void initArea();
 void initMario();
 void updateController(OSContPad *input);
