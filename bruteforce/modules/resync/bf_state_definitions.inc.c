0e3f59c0113479653132e37dd9983ab2432f2f18
FramePerfection <Nafoizret@web.de>
2024-02-10 16:00:39 +0100

    attempt to resync further than greedy sync can

--- bruteforce/modules/resync/bf_state_definitions.inc.c
+++ bruteforce/modules/resync/bf_state_definitions.inc.c
@@ -14,16 +14,19 @@ extern s16 gCurrCourseNum;
 
 #else
 
-// scoring parameters
+// options
+BF_STATIC_STATE(boolean, auto_detect_reference_offset, bfStaticState.auto_detect_reference_offset, "If set, the last reference frame that scores the smallest difference to the initial state will be chosen as the reference start.")
 BF_STATIC_STATE(boolean, relative_frame_numbers, bfStaticState.relative_frame_numbers, "If set, frame numbers will be interpreted as relative to 'm64_start'.")
-BF_STATIC_STATE(boolean, per_frame_matching, bfStaticState.per_frame_matching, "If set, a best input will exhaustively be searched for each frame one after the other before starting the finetuning loop.")
-BF_STATIC_STATE(boolean, match_reference_buttons, bfStaticState.match_reference_buttons, "If set, buttons will be pressed exactly as in the reference run, ignoring the base run's button inputs")
+BF_STATIC_STATE(boolean, match_reference_buttons, bfStaticState.match_reference_buttons, "If set, buttons will be pressed exactly as in the reference run, ignoring the base run's button inputs.")
 BF_STATIC_STATE(u32, reference_offset, bfStaticState.reference_offset, "The offset into the reference run to start comparing at. TODO: example")
-BF_STATIC_STATE(f64, x_weight, bfStaticState.x_weight, "The weight of Mario's x-position matching.")
-BF_STATIC_STATE(f64, y_weight, bfStaticState.y_weight, "The weight of Mario's y-position matching.")
-BF_STATIC_STATE(f64, z_weight, bfStaticState.z_weight, "The weight of Mario's z-position matching.")
-BF_STATIC_STATE(f64, angle_weight, bfStaticState.angle_weight, "The weight of Mario's angle difference in angular units. Should be drastically lower than x/y/z weights.")
-BF_STATIC_STATE(f64, hspeed_weight, bfStaticState.hspeed_weight, "The weight of Mario's horizontal speed matching.")
+BF_STATIC_STATE(u32, num_fine_tuning_frames, bfStaticState.num_fine_tuning_frames, "The number of frames to fine-tune with a genetic algorithm at the end of the per-frame matched result.")
+
+// scoring parameters
+BF_CONTROL_STATE(f64, x_weight, "The weight of Mario's x-position matching.")
+BF_CONTROL_STATE(f64, y_weight, "The weight of Mario's y-position matching.")
+BF_CONTROL_STATE(f64, z_weight, "The weight of Mario's z-position matching.")
+BF_CONTROL_STATE(f64, angle_weight, "The weight of Mario's angle difference in angular units. Should be drastically lower than x/y/z weights.")
+BF_CONTROL_STATE(f64, hspeed_weight, "The weight of Mario's horizontal speed matching.")
 __NL__
 
 BF_STATIC_STATE(s16, level_num, gCurrLevelNum, "TODO: lol")
